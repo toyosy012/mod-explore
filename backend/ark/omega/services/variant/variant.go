3e70087389b36b785d6ea0dc16381623e7e21450
toyosy012 <toyosy012@gmail.com>
2024-01-08 02:33:55 +0900

    feat: AddVariantType-#5
    バリアント型を定義
    
    グループ名とバリアンと名を持たせる

---
+++ backend/ark/omega/services/variant/variant.go
@@ -0,0 +1,20 @@
+package variant
+
+type GroupName string
+
+type Name string
+
+type Variant struct {
+	group GroupName
+	name  Name
+}
+
+func NewVariant(group GroupName, name Name) Variant {
+	return Variant{
+		group: group,
+		name:  name,
+	}
+}
+
+func (v Variant) Group() GroupName { return v.group }
+func (v Variant) Name() Name       { return v.name }
