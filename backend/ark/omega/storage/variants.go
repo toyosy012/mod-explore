70107078111ab6f4bde02c440b76b258105518ac
toyosy012 <toyosy012@gmail.com>
2024-02-08 08:52:03 +0900

    feat: AddVariantCRUD-#8 DBアクセスモジュール追加
    バリアントテーブルにアクセスするためのモジュールを追加
    
    グループも別テーブルで管理するので現状ではモデルのみ定義

---
+++ backend/ark/omega/storage/variants.go
@@ -0,0 +1,62 @@
+package storage
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/jmoiron/sqlx"
+
+	"mods-explore/ark/omega/logic/variant/domain/model"
+	"mods-explore/ark/omega/logic/variant/domain/service"
+)
+
+type VariantClient struct {
+	*sqlx.DB
+}
+
+func NewVariantClient(dsn string) (service.VariantRepository, error) {
+	db, err := sqlx.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	return VariantClient{
+		DB: db,
+	}, nil
+}
+
+// variantModel Listでも1度に取得されるレコード量は決まっているので、domain modelで異なるバインド用モデルを定義する
+type variantModel struct {
+	ID    int        `db:"id"`
+	Name  string     `db:"name"`
+	Group groupModel `db:"group"`
+}
+
+func (v VariantClient) FindVariant(ctx context.Context, id model.VariantID) (*model.Variant, error) {
+	query, args, err := v.BindNamed(`SELECT * FROM variants WHERE id = :id`, map[string]any{"id": id})
+	if err != nil {
+		return nil, err
+	}
+
+	var row variantModel
+	if err = v.GetContext(ctx, &row, query, args); errors.Is(err, sql.ErrNoRows) {
+		return nil, service.NotFound
+	} else if err != nil {
+		return nil, err
+	}
+
+	var variant = model.NewVariant(
+		model.VariantID(row.ID),
+		model.GroupName(row.Group.Name),
+		model.Name(row.Name),
+	)
+	return &variant, nil
+}
+func (v VariantClient) ListVariants(context.Context) (model.Variants, error) { return nil, nil }
+func (v VariantClient) CreateVariant(context.Context, service.CreateVariant) (*model.Variant, error) {
+	return nil, nil
+}
+func (v VariantClient) UpdateVariant(context.Context, service.UpdateVariant) (*model.Variant, error) {
+	return nil, nil
+}
+func (v VariantClient) DeleteVariant(context.Context, model.VariantID) error { return nil }
