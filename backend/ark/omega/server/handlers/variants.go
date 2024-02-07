a95262adb3ea4f6c1597c6562fd5ae261b16a183
toyosy012 <toyosy012@gmail.com>
2024-02-08 08:40:01 +0900

    feat: AddVariantCRUD-#8 コントローラのハンドラーを追加
    バリアントレコードを取得するEchoのハンドラーを追加
    
    ユースケースインターフェースに依存させる
    JSONに変換できる入出力用の構造体を定義

---
+++ backend/ark/omega/server/handlers/variants.go
@@ -0,0 +1,162 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+	"mods-explore/ark/omega/logic/variant/domain/model"
+	"mods-explore/ark/omega/logic/variant/domain/service"
+	"mods-explore/ark/omega/logic/variant/usecase"
+)
+
+type Variant struct {
+	usecase.VariantUsecase
+}
+
+func NewVariant(usecase usecase.VariantUsecase) VariantHandler {
+	return Variant{
+		usecase,
+	}
+}
+
+type VariantHandler interface {
+	Read(echo.Context) error
+	List(echo.Context) error
+	Create(echo.Context) error
+	Update(echo.Context) error
+	Delete(echo.Context) error
+}
+
+type referenceParams struct {
+	variantID int `param:"id" validator:"required"`
+}
+
+func (v Variant) Read(c echo.Context) error {
+	var params referenceParams
+	if err := c.Bind(&params); err != nil {
+		return err
+	}
+
+	variant, err := v.VariantUsecase.Find(c.Request().Context(), model.VariantID(params.variantID))
+	if err != nil {
+		return err
+	}
+
+	if err = c.JSON(http.StatusOK, NewVariantValue(*variant)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v Variant) List(c echo.Context) error {
+	variants, err := v.VariantUsecase.List(c.Request().Context())
+	if err != nil {
+		return err
+	}
+	if err = c.JSON(http.StatusOK, NewVariantValues(variants)); err != nil {
+		return err
+	}
+	return nil
+}
+
+type createBody struct {
+	Group string `json:"group" validate:"required"`
+	Name  string `json:"name" validate:"required"`
+}
+
+func (v Variant) Create(c echo.Context) error {
+	var body createBody
+	if err := c.Bind(&body); err != nil {
+		return err
+	}
+
+	variant, err := v.VariantUsecase.Create(
+		c.Request().Context(),
+		service.NewCreateVariant(
+			model.GroupName(body.Group),
+			model.Name(body.Name),
+		),
+	)
+	if err != nil {
+		return err
+	}
+
+	if err = c.JSON(http.StatusOK, NewVariantValue(*variant)); err != nil {
+		return err
+	}
+	return nil
+}
+
+type updateBody struct {
+	ID    int    `json:"id" validate:"required"`
+	Group string `json:"group" validate:"required"`
+	Name  string `json:"name" validate:"required"`
+}
+
+func (v Variant) Update(c echo.Context) error {
+	var body updateBody
+	if err := c.Bind(&body); err != nil {
+		return err
+	}
+
+	variant, err := v.VariantUsecase.Update(
+		c.Request().Context(),
+		service.NewUpdateVariant(model.VariantID(body.ID), model.GroupName(body.Group), model.Name(body.Name)),
+	)
+	if err != nil {
+		return err
+	}
+
+	if err = c.JSON(http.StatusOK, NewVariantValue(*variant)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v Variant) Delete(c echo.Context) error {
+	var params referenceParams
+	if err := c.Bind(&params); err != nil {
+		return err
+	}
+
+	err := v.VariantUsecase.Delete(c.Request().Context(), model.VariantID(params.variantID))
+	if err != nil {
+		return err
+	}
+
+	if err = c.JSON(http.StatusOK, map[string]any{}); err != nil {
+		return err
+	}
+	return nil
+}
+
+type VariantValue struct {
+	ID    model.VariantID `json:"ID" validator:"required"`
+	Name  model.Name      `json:"Name" validator:"required"`
+	Group model.GroupName `json:"Group" validator:"required"`
+}
+
+func NewVariantValue(v model.Variant) VariantValue {
+	return VariantValue{
+		ID:    v.ID(),
+		Name:  v.Name(),
+		Group: v.Group(),
+	}
+}
+
+type VariantValues []VariantValue
+
+func NewVariantValues(vs []model.Variant) (values VariantValues) {
+	for _, v := range vs {
+		values = append(
+			values,
+			VariantValue{
+				ID:    v.ID(),
+				Name:  v.Name(),
+				Group: v.Group(),
+			},
+		)
+	}
+	return values
+}
