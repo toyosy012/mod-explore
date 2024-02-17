c189da21a2c95aae2a3436c4097d369bf534b94c
toyosy012 <toyosy012@gmail.com>
2024-02-17 19:09:45 +0900

    feat: AddVariantTest-#40 バリアントのFindテスト追加
    モックにリポジトリからの戻り値を定義して、ユースケースのテストを行う

--- backend/ark/omega/logic/variant/usecase/variant_test.go
+++ backend/ark/omega/logic/variant/usecase/variant_test.go
@@ -1,11 +1,16 @@
 package usecase
 
 import (
+	"context"
+	"errors"
 	"testing"
 
+	"github.com/morikuni/failure"
 	"github.com/samber/do"
 	"github.com/stretchr/testify/suite"
 
+	"mods-explore/ark/omega/logic"
+	"mods-explore/ark/omega/logic/variant/domain/model"
 	"mods-explore/ark/omega/logic/variant/domain/service"
 )
 
@@ -22,6 +27,19 @@ func TestVariantSuite(t *testing.T) {
 	suite.Run(t, newTestVariantSuite())
 }
 
+var (
+	ctx  = context.Background()
+	find = "FindVariant"
+	e    = errors.New("test")
+)
+
+const (
+	id = iota
+	notExistID
+	intervalServerErrID
+	errID
+)
+
 func (s *VariantTestSuite) SetupSuite() {
 	injector := do.New()
 
@@ -34,4 +52,53 @@ func (s *VariantTestSuite) SetupSuite() {
 	}
 
 	s.usecase = usecase
+}
+
+func (s *VariantTestSuite) TestFind() {
+	variant := model.NewVariant(model.VariantID(id), "cosmic", "meteor")
+	{
+		s.mockDB.On(
+			find,
+			ctx,
+			model.VariantID(id),
+		).
+			Return(&variant, nil)
+		r, err := s.usecase.Find(ctx, model.VariantID(id))
+		if err != nil {
+			s.T().Error(err)
+			return
+		}
+
+		s.Equal(&variant, r)
+	}
+	{
+		s.mockDB.On(
+			find,
+			ctx,
+			model.VariantID(notExistID),
+		).
+			Return(nil, service.NotFound)
+		_, err := s.usecase.Find(ctx, model.VariantID(notExistID))
+		s.True(failure.Is(err, logic.NotFound))
+	}
+	{
+		s.mockDB.On(
+			find,
+			ctx,
+			model.VariantID(intervalServerErrID),
+		).
+			Return(nil, service.IntervalServerError)
+		_, err := s.usecase.Find(ctx, model.VariantID(intervalServerErrID))
+		s.True(failure.Is(err, logic.IntervalServerError))
+	}
+	{
+		s.mockDB.On(
+			find,
+			ctx,
+			model.VariantID(errID),
+		).
+			Return(nil, failure.Wrap(e))
+		_, err := s.usecase.Find(ctx, model.VariantID(errID))
+		s.True(errors.Is(err, e))
+	}
 }
