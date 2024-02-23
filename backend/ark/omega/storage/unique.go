5e113987b6bf7f8e7adf508c5a65077c7b2dfabd
toyosy012 <toyosy012@gmail.com>
2024-02-23 11:15:25 +0900

    feat: AddUniqueDinosaurCommand-#7 コマンドリポジトリにインターフェースの実装を定義
    メソッドを追加

--- backend/ark/omega/storage/unique.go
+++ backend/ark/omega/storage/unique.go
@@ -1,5 +1,12 @@
 package storage
 
+import (
+	"context"
+
+	"mods-explore/ark/omega/logic/creature/domain/model"
+	"mods-explore/ark/omega/logic/creature/domain/service"
+)
+
 type UniqueModel struct {
 	ID               int     `db:"id"`
 	Name             string  `db:"name"`
@@ -9,4 +16,16 @@ type UniqueModel struct {
 
 type UniqueCommandRepo struct {
 	Client[UniqueModel, int]
+}
+
+func (r UniqueCommandRepo) Insert(context.Context, service.CreateUniqueDinosaur) (model.UniqueDinosaurID, error) {
+	return model.UniqueDinosaurID(0), nil
+}
+
+func (r UniqueCommandRepo) Update(context.Context, service.UpdateUniqueDinosaur) error {
+	return nil
+}
+
+func (r UniqueCommandRepo) Delete(context.Context, model.UniqueDinosaurID) error {
+	return nil
 }
